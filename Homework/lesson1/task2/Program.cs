497a1364f6cd1713a3d4910b9023182fe82534bf
Eduard <eduard_mayburov@mail.ru>
2022-11-19 09:09:34 +0700

    second Homework task done

---
+++ Homework/lesson1/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Imput first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Imput first number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Imput first number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (max < num2)
+{
+    max = num2;
+    if (max < num3)
+    {
+        max = num3;
+    }
+}
+Console.Write($"max = {max}");
