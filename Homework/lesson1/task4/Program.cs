52b52632c694fd871831016c0a9e72b9a57ab1cc
Eduard <eduard_mayburov@mail.ru>
2022-11-19 10:38:42 +0700

    Fourth Homework task done

---
+++ Homework/lesson1/task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Imput number");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (num > 0)
+{
+   
+    while (count <= num)
+        {
+        Console.WriteLine($"{count}");
+        count = count + 2;
+        }
+}
+
+else
+{
+    num = num - 1;
+        while (count <= num)
+        {
+        Console.WriteLine($"{count}");
+        count = count + 2;
+        }
+}
