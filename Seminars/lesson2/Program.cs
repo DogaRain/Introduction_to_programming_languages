405ef79f42b67bdaee16374380daa7cf38dd7f5f
Eduard <eduard_mayburov@mail.ru>
2022-11-22 06:49:26 +0700

    lesson2 third task done

--- Seminars/lesson2/Program.cs
+++ Seminars/lesson2/Program.cs
@@ -30,7 +30,7 @@ Console.WriteLine($"The biggest digit -> {result}");*/
 //782 -> 72
 //918 -> 98
 
-int CutNumber (int num)
+/*int CutNumber (int num)
 {
     int ed = num % 10;
     int sot = num / 100;
@@ -40,4 +40,26 @@ int CutNumber (int num)
 
 int random = new Random().Next(100,1000);
 int newNumber = CutNumber(random);
-Console.WriteLine($"New version of {random} is {newNumber}");
+Console.WriteLine($"New version of {random} is {newNumber}");*/
+
+//Задача 3.Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.
+
+//34, 5 -> не кратно, остаток 4
+//16, 4 -> кратно
+
+void main (int num1, int num2)
+{
+    if (num1 % num2 == 0)
+    {
+        Console.WriteLine($"{num1}, {num2} -> Кратно");
+    }
+    else
+    {
+        int num3 = num1 % num2;
+        Console.WriteLine($"{num1}, {num2} некратно, остаток {num3}");
+    }
+    
+}
+int random1 = new Random().Next(1,100);
+int random2 = new Random().Next(1,100);
+main(random1, random2);
