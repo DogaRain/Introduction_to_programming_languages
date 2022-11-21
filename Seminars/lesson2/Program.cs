d44b472a13a4c058e8e02751b8927e36c0726f83
Eduard <eduard_mayburov@mail.ru>
2022-11-22 06:29:35 +0700

    lesson2 second task done

--- Seminars/lesson2/Program.cs
+++ Seminars/lesson2/Program.cs
@@ -2,7 +2,7 @@
 //78 -> 8
 //12-> 2
 //85 -> 8
-
+/*
 int FindBiggestDigit (int num)
 {
     int ed = num % 10;
@@ -22,4 +22,22 @@ int random = new Random().Next(10, 100);
 int result = FindBiggestDigit(random);
 
 Console.WriteLine($"your number -> {random}");
-Console.WriteLine($"The biggest digit -> {result}");
+Console.WriteLine($"The biggest digit -> {result}");*/
+
+//2 Задача.Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int CutNumber (int num)
+{
+    int ed = num % 10;
+    int sot = num / 100;
+    int res = sot * 10 + ed;
+    return res; 
+}
+
+int random = new Random().Next(100,1000);
+int newNumber = CutNumber(random);
+Console.WriteLine($"New version of {random} is {newNumber}");
