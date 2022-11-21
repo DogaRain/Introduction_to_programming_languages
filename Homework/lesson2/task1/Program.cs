43e42119d1a486aa2775560ff95757636963d0c7
Eduard <eduard_mayburov@mail.ru>
2022-11-22 05:57:39 +0700

    commit1.2

---
+++ Homework/lesson2/task1/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Imput number");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine (Math.Abs(num) % 100);
---
+++ Seminars/lesson2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 1. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//78 -> 8
+//12-> 2
+//85 -> 8
+
+int FindBiggestDigit (int num)
+{
+    int ed = num % 10;
+    int dec = num / 10;
+    if (ad > dec)
+    {
+        return ed;
+    }
+    else
+    {
+        return dec;
+    }
+}
+
+int random = new Random().Next(10, 100);
+
+int result = FindBiggestDigit(random);
