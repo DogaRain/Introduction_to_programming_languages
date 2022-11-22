205b2835b76b866beb5af4f1341348632e2f4dc3
Eduard <eduard_mayburov@mail.ru>
2022-11-22 08:27:59 +0700

    HW2 task3 done

---
+++ Homework/lesson2/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿bool main (int num)
+{
+    if (num > 5)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+
+}
+int random = new Random().Next(1, 7);
+main (random);
+Console.WriteLine($"{random} - {main(random)}");
