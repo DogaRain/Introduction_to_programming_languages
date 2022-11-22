68c6aa9431d7b9824f814b94826d2ef036bed22c
Eduard <eduard_mayburov@mail.ru>
2022-11-22 08:11:50 +0700

    HW2 task2 done

---
+++ Homework/lesson2/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿void FindThirdNum (int num)
+{
+    if (num > 99 && num < 1000)
+    {
+        int num2 = num % 10;
+        Console.WriteLine($"third num -> {num2}");  
+    }
+    else
+    {
+       Console.WriteLine("not a three-digit number");
+    }
+}
+int random = new Random().Next(1, 1000);
+
+FindThirdNum (random);
+
+Console.WriteLine($"Your number -> {random}");
