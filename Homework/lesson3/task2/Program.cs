57a401674c880be30c2679fa435f876bb0145e3b
Eduard <eduard_mayburov@mail.ru>
2022-11-23 07:10:39 +0700

    HW3 task2 done

---
+++ Homework/lesson3/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿double FindLegth(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double Len = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+    return Len;
+}
+Console.WriteLine("Введите номер координат X1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер координат Y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер координат Z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер координат X2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер координат Y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер координат z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindLegth(x1, y1, z1, x2, y2, z2));
