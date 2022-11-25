7b56ab6665648fba71697c4d5ef2d6617ef5016a
Eduard <eduard_mayburov@mail.ru>
2022-11-25 11:34:50 +0700

    hm4 task1 done

---
+++ Homework/lesson4/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int  NumDegree (int A, int B)
+{
+    int count = 1;
+    int prod = 1;
+    while (count <= B)
+    {
+        prod = prod * A;
+        count++;
+    }
+    Console.Write($"{A} in {B} degree =");
+    return prod;
+}
+Console.WriteLine ("Input number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input Degree");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = NumDegree(num1, num2);
+Console.Write($" {result}");
