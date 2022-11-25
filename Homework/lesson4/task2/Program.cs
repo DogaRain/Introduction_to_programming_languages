b4cea813f8bf92f20b5c19f4faaa0983593a9614
Eduard <eduard_mayburov@mail.ru>
2022-11-25 12:17:15 +0700

    hm4 task2 done

---
+++ Homework/lesson4/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int Sum (int N)
+{
+    int sum = 0;
+    while (N > 0)
+    {
+    int ed = N % 10;
+    N = N / 10;
+    sum = sum + ed; 
+    } 
+    return sum;
+}
+Console.WriteLine("Input number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"numbers sum of {a} = {Sum(a)}");
