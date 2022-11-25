f7627d13f5edbad83d13fe1e34c76e0ff4198a9d
Eduard <eduard_mayburov@mail.ru>
2022-11-25 12:21:46 +0700

    hm4 task3 done

---
+++ Homework/lesson4/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿ int[] CreateRandArray (int size, int minValue, int maxValue)
+{
+    int [] newArray = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue+1);
+        
+    }
+    return newArray;
+}
+void ShowArray (int[] array)
+{
+    Console.Write("Resulting array -> ");
+        for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+}
+Console.WriteLine ("Input array size");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array min");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array max");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandArray(a,min,max);
+ShowArray(myArray);
