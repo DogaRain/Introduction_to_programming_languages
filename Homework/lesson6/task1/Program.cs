db53a1409d8eff8c007879302ddc845bf0932c79
Eduard <eduard_mayburov@mail.ru>
2022-11-29 11:40:15 +0700

    hm6 task1 done

---
+++ Homework/lesson6/task1/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] CreateRandArray (int size, int minValue, int maxValue)
+{
+    int [] newArray = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue+1);
+        
+    }
+    return newArray;
+}
+void ShowArray (int[] array)
+{
+    Console.Write("Resulting array -> ");
+        for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+}
+void PositiveNumbers (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        { 
+        count++;
+        }
+    }
+    Console.WriteLine($"in array {count} positive numbers");
+}
+Console.WriteLine ("Input array size");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array min");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array max");
+int c = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandArray(a,b,c);
+ShowArray(myArray);
+PositiveNumbers(myArray);
