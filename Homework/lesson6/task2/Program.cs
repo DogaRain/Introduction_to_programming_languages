f2f3865c719051fabbfadd20fe76535f708e3add
Eduard <eduard_mayburov@mail.ru>
2022-11-29 12:39:51 +0700

    hm6 task2 done

--- Homework/lesson6/task2/Program.cs
+++ Homework/lesson6/task2/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿void FindIntersection (double k1, double b1, double k2, double b2)
+
+{
+    double x = (b2 - b1 ) / (k1 - k2);
+    double y = k1 * (b2 - b1) / ( k1 - k2) + b1;
+
+    Console.Write($"the point of intersection of two straight lines is {(x, y)}");
+}
+
+Console.WriteLine("Input k1");
+double a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input b1");
+double b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input k2");
+double c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input b2");
+double d = Convert.ToInt32(Console.ReadLine());
+
+FindIntersection(a,b,c,d);
