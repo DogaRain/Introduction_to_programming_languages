e0249113fee922dbb3ecf2b6e9a849e02305b3a4
Eduard <eduard_mayburov@mail.ru>
2022-11-30 12:20:46 +0700

    hm7 task 1 done

--- Homework/lesson7/task1/Program.cs
+++ Homework/lesson7/task1/Program.cs
@@ -1,10 +1,11 @@
-﻿double [,] Create2DRandArray (double minValue, double maxValue, int rows, int colums)
+﻿double [,] Create2DRandArray (int rows, int colums)
 {
     double[,] newArray = new double[rows,colums];
     for (int i = 0; i < rows; i++)
         for (int j = 0; j < colums; j++)
         {
-            newArray[i,j] = new Convert.ToDouble(Random.Next(-100, 100)/10.0);
+            newArray[i,j] = new Random().NextDouble() * 10;
+            newArray[i,j] = Math.Round(newArray[i,j], 2);
         }    
         return newArray;
 }
@@ -23,8 +24,5 @@ Console.WriteLine ("Input array rows");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Input array colums");
 int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Input array min");
-double min = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine ("Input array max");
-double max = Convert.ToDouble(Console.ReadLine());
-Show2DArray(Create2DRandArray(min,max,a,b));
+
+Show2DArray(Create2DRandArray(a,b));
