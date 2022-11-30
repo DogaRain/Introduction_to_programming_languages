41d947cfc5c38d6e363f8af9fa5ab8542678215d
Eduard <eduard_mayburov@mail.ru>
2022-11-30 10:27:04 +0700

    lesson7 end

---
+++ Seminars/lesson7/Program.cs
@@ -0,0 +1,165 @@
+﻿/*int [,] Create2DRandArray (int minValue, int maxValue, int rows, int colums)
+{
+    int[,] newArray = new int[rows,colums];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue, maxValue+1);
+        }    
+        return newArray;
+}
+void Show2DArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] +" ");
+        } 
+        Console.WriteLine();
+    }    
+}
+Console.WriteLine ("Input array rows");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array colums");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array min");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array max");
+int max = Convert.ToInt32(Console.ReadLine());
+Show2DArray(Create2DRandArray(min,max,a,b));*/
+
+//Задача 2. ****Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+// Выведите полученный массив на экран.
+
+/*int [,] Create2DRandArray (int rows, int colums)
+{
+    int[,] newArray = new int[rows,colums];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+        {
+            newArray[i,j] = i+j;
+        }    
+        return newArray;
+}
+void Show2DArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] +" ");
+        } 
+        Console.WriteLine();
+    }    
+}
+Console.WriteLine ("Input array rows");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array colums");
+int b = Convert.ToInt32(Console.ReadLine());
+Show2DArray(Create2DRandArray (a,b));*/
+
+//Задача 3.Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+//Например, изначально массив выглядел вот так:
+
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Новый массив будет выглядеть вот так:
+
+//1 4 7 2
+//5 **81 ** 2 **9**
+//8 4 2 4
+
+/*int [,] Create2DRandArray (int rows, int colums)
+{
+    int[,] newArray = new int[rows,colums];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+        {
+            newArray[i,j] = i+j;
+        }    
+        return newArray;
+}
+int[,] Squart(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            array[i,j] = array[i,j] * array[i,j];
+        }    
+        return array;    
+}
+void Show2DArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] +" ");
+        } 
+        Console.WriteLine();
+    }
+    Console.WriteLine();    
+}
+
+Console.WriteLine ("Input array rows");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array colums");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = Create2DRandArray(a,b);
+Show2DArray(array);
+Show2DArray(Squart(array));*/
+
+//Задача 4. Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+//Например, задан массив:
+
+int [,] Create2DRandArray (int minValue, int maxValue, int rows, int colums)
+{
+    int[,] newArray = new int[rows,colums];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue, maxValue+1);
+        }    
+        return newArray;
+}
+
+int SumDiag(int[,]array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        result = result + array[i,i];
+    }
+        return result;    
+}
+void Show2DArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] +" ");
+        } 
+        Console.WriteLine();
+    }
+    Console.WriteLine();    
+}
+
+Console.WriteLine ("Input array rows");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array colums");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array min");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array max");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = Create2DRandArray(min,max,a,b);
+Show2DArray(array);
+Console.WriteLine(SumDiag(array));
