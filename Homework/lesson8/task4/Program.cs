8e0eb1d72227790079881eb6a5a20cee982d2097
Eduard <eduard_mayburov@mail.ru>
2022-12-04 00:02:54 +0700

    hm8 task5 done

---
+++ Homework/lesson8/task4/Program.cs
@@ -0,0 +1,35 @@
+﻿int [,,] Create2DRandArray (int row, int row2, int colums)
+{
+    int[,,] newArray = new int[row,row2,colums];
+    for (int i = 0; i < row; i++)
+    for (int p = 0; p < row; p++)
+        for (int j = 0; j < colums; j++)
+        {
+            newArray[i,p,j] = new Random().Next(10, 100);
+        }    
+        return newArray;
+}
+
+void Show3DArray (int [,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int p = 0; p < array.GetLength(1); p++)
+        {   
+            for (int j = 0; j < array.GetLength(2); j++)
+            {
+                Console.Write(array[i,p,j]+$" [{i},{p},{j}]"+" ");
+            } 
+        Console.WriteLine();
+        }    
+}
+
+
+
+Console.WriteLine ("Input array first rows");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array second rows");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input array colums");
+int c = Convert.ToInt32(Console.ReadLine());
+int[,,] MyArray = Create2DRandArray(a,b,c);
+Show3DArray(MyArray);
---
+++ Homework/lesson8/task5/Program.cs
@@ -0,0 +1,36 @@
+﻿int [,] CreateArray(int size)
+{
+int[,] newArray = new int[size,size];
+int num = 1;
+int i = 0;
+int j = 0;
+
+    while (num <= size * size)
+    {
+        newArray[i, j] = num;
+        if (i <= j + 1 && i + j < size - 1)
+            ++j;
+        else if (i < j && i + j >= size - 1)
+            ++i;
+        else if (i >= j && i + j > size - 1)
+            --j;
+        else
+            --i;
+        ++num;
+        }
+        return newArray;
+}
+void Show2DArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] +" ");
+        } 
+        Console.WriteLine();
+    }    
+}
+Console.WriteLine ("Input array size");
+int a = Convert.ToInt32(Console.ReadLine());
+Show2DArray(CreateArray(a));
